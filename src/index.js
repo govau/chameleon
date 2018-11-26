ab95502398b39ee03d73fa54d6d313bf19fe225e
Alex Page <alex@alexpage.com.au>
2018-11-26 12:58:08 +1100

    Adjusting spacing

--- src/index.js
+++ src/index.js
@@ -1,64 +1,67 @@
-const express = require('express')
-const helmet = require('helmet');
-const path = require("path");
-const sass = require("node-sass");
-const fs = require("fs");
-const urldecode = require('urldecode')
+const express = require( 'express' );
+const helmet = require( 'helmet' );
+const path = require( 'path' );
+const sass = require( 'node-sass' );
+const fs = require( 'fs' );
+const urldecode = require( 'urldecode' )
 
 const app = express();
 
-const port = process.env.PORT || 3000
-const uikitSass = fs.readFileSync( './pancake.scss', 'utf-8' );
+const port = process.env.PORT || 3000;
+const uikitSass = fs.readFileSync( '../templates/pancake.scss', 'utf-8' );
 
-app.use(helmet());
+app.use( helmet() );
 
-// Static endpoint for developer testing, disabled in production
-process.env.NODE_ENV === "production" ? undefined : app.use('/test', express.static(path.join(__dirname, '/../test/system')))
+// Static endpoint for developer testing
+process.env.NODE_ENV === 'production'
+	? undefined
+	: app.use( '/test', express.static( path.join( __dirname, '/../test/system' ) ) );
 
 /**
  * Static endpoint for the full-page template, 
  * this will resolve the index.html in the folder.
 */ 
-app.use('/templates/full-page', express.static(path.join(__dirname, '/../templates/full-page')))
+app.use( '/templates/full-page', express.static( path.join( __dirname, '/../templates/full-page') ) );
 
-app.get("/frame",  async ( req, res ) => {
-    const colors = req.query;
-    
-    const colorMap = {
-        text:           '$AU-color-foreground-text',
-        action:         '$AU-color-foreground-action',
-        focus:          '$AU-color-foreground-focus',
-        background:     '$AU-color-background',
-        textDark:       '$AU-colordark-foreground-text',
-        actionDark:     '$AU-colordark-foreground-action',
-        focusDark:      '$AU-colordark-foreground-focus',
-        backgroundDark: '$AU-colordark-background',
-    };
+app.get( '/frame',  async ( request, response ) => {
+	const colors = request.query;
 
-    let customStyles = '';
+	const colorMap = {
+		text:           '$AU-color-foreground-text',
+		action:         '$AU-color-foreground-action',
+		focus:          '$AU-color-foreground-focus',
+		background:     '$AU-color-background',
+		textDark:       '$AU-colordark-foreground-text',
+		actionDark:     '$AU-colordark-foreground-action',
+		focusDark:      '$AU-colordark-foreground-focus',
+		backgroundDark: '$AU-colordark-background',
+	};
 
-    Object.keys( colors ).map( ( colorType ) => {
-        const colorValue = colors[ urldecode( colorType ) ];
+	let customStyles = '';
 
-        if( colorValue ) {
-            customStyles += `${ colorMap[ colorType ] }: ${ colors[ colorType ] };\n`;
-        }
-    });
+	Object.keys( colors ).map( ( colorType ) => {
+		const colorValue = colors[ colorType ];
 
-    const css = sass.renderSync({
-        data:         customStyles + uikitSass,
-        outputStyle: 'compressed',
-    });
+		if( colorValue ) {
+			customStyles += `${ colorMap[ colorType ] }: ${ colors[ colorType ] };\n`;
+		}
+	});
 
-    /**
-     * @todo Take me to the page I was on, not index.html
-     */
-    const html = (await fs.readFileSync(path.join(__dirname, "/../templates/full-page/index.html"), "utf-8"))
-        .replace("<!--INJECTED STYLES-->",`<style>${css.css}</style>`);
+	const css = sass.renderSync({
+		data:         customStyles + uikitSass,
+		outputStyle: 'compressed',
+	});
 
-    res.send(html);
+	/**
+	 * @todo Take me to the page I was on, not index.html
+	 */
+	const templateLocation = path.join( __dirname, '/../templates/full-page/index.html' );
+	const template = fs.readFileSync( templateLocation, 'utf-8' );
+	const html = template.replace( '<!--INJECTED STYLES-->', `<style>${ css.css }</style>` );
+
+	response.send( html );
 });
 
-app.listen(port)
+app.listen( port, () => console.log( `Listening on http://localhost:${ port } ...` ) );
 
 module.exports = app;
