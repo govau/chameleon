b8349857058f034b6c2a209b0aa1e903f85bc3b7
Adam A. Zerella <adamzerella@users.noreply.github.com>
2018-11-28 09:30:20 +1100

    Refactored project structure
    Added inital fixture for testing

--- test/integration/index.spec.js
+++ test/integration/index.spec.js
@@ -1,17 +1,17 @@
 const server = require("../../src/index");
 const chai = require("chai");
-const chaiHTTP = require("chai-http");
+const request = require("supertest");
+const fs = require("fs");
 
-chai.use( chaiHTTP );
-chai.should();
+const expect = chai.expect;
 
 describe( "Server", () => {
 	describe( "GET /", () => {
 		it( "Should return a 404", ( done ) => {
-			chai.request( server )
+			request( server )
 				.get("/")
 				.end( ( error, response ) => {
-					response.should.have.status(404);
+					expect( response.status ).to.equal( 404 );
 					done();
 				});
 		})
@@ -19,47 +19,117 @@ describe( "Server", () => {
 
 	describe( "GET /main.js", () => {
 		it( "Should return a 200", ( done ) => {
-			chai.request( server )
+			request( server )
 				.get("/main.js")
 				.end( ( error, response ) => {
-					response.should.have.status(200);
+					expect( response.status ).to.equal( 200 );
 					done();
 				});
 		})
-	})
-	
-	describe( "GET /frame", () => {
-		it( "Should return a 200", ( done ) => {
-			chai.request( server )
-				.get("/frame")
+
+		it( "Should return a javascript file", ( done ) => {
+			request( server )
+				.get("/main.js")
 				.end( ( error, response ) => {
-					response.should.have.status(200);
+					expect( response.type ).to.equal( "application/javascript" );
 					done();
 				});
-		})
+		});
 	})
 
-	describe( "GET /templates", () => {
+	describe( "GET /frame", () => {
 		it( "Should return a 200", ( done ) => {
-			chai.request( server )
-				.get("/templates")
+			request( server )
+				.get("/frame")
 				.end( ( error, response ) => {
-					response.should.have.status(200);
+					expect( response.status ).to.equal( 200 );
+					done();
+				});
+		});
+
+		it( "Should return a html file", ( done ) => {
+			request( server )
+				.get("/frame")
+				.end( ( error, response ) => {
+					expect( response.type ).to.equal( "text/html" );
+					done();
+				});
+		});
+
+		it( "Should return a 200 with text colour red", ( done ) => {
+			request( server )
+				.get("/frame?text=red")
+				.end( ( error, response ) => {
+					expect( response.status ).to.equal( 200 );
+					/**
+					 * @todo CHECK COLOUR IS RED ??
+					 */
+					done();
+				});
+		});
+
+		it( "Should return a 200 with full empty query string", ( done ) => {
+			request( server )
+				.get("/frame?text=&action=&focus=&background=&textDark=&actionDark=&focusDark=&backgroundDark=")
+				.end( ( error, response ) => {
+					expect( response.status ).to.equal( 200 );
+					/**
+					 * @todo CHECK COLOUR IS RED ??
+					 */
+					done();
+				});
+		});
+
+		it( "Should return a 200 with full query string", ( done ) => {
+			request( server )
+				.get("/frame?text=red&action=red&focus=red&background=red&textDark=red&actionDark=red&focusDark=red&backgroundDark=red")
+				.end( ( error, response ) => {
+					expect( response.status ).to.equal( 200 );
+					/**
+					 * @todo CHECK COLOUR IS RED ??
+					 */
+					done();
+				});
+		});
+
+		it( "Should return a 200 from malformed querystring to /frame", ( done ) => {
+			request( server )
+				.get("/frame?texABCD")
+				.end( ( error, response ) => {
+					expect( response.status ).to.equal( 200 );
+					done();
+				});
+		});
+	})
+
+	describe( "GET /templates/index.html", () => {
+		it( "Should return a 200", ( done ) => {
+			request( server )
+				.get("/templates/index.html")
+				.end( ( error, response ) => {
+					expect( response.status ).to.equal( 200 );
+					done();
+				});
+		})
+
+		it( "Should return a html file", ( done ) => {
+			request( server )
+				.get("/templates/index.html")
+				.end( ( error, response ) => {
+					expect( response.type ).to.equal( "text/html" );
 					done();
 				});
 		})
 	})
 
 	describe( "GET /test", () => {
-		it( "Should return a 200 if NODE_ENV isn't set to prod", ( done ) => {
-			chai.request( server )
-				.get("/test")
+		it( "Should return a 200 if NODE_ENV isn't set to 'production'", ( done ) => {
+			request( server )
+				.get("/test/")
 				.end( ( error, response ) => {
-					response.should.have.status(200);
+					expect( response.status ).to.equal( 200 );
 					done();
 				});
 		})
 	})
 });
-
-// process.exit(0);
