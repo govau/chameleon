d43bcd3afaffa9b1e8bab58ad1e54cab7cab886b
Adam A. Zerella <adamzerella@users.noreply.github.com>
2018-11-28 13:00:52 +1100

    Refreshed fixtures

--- src/index.js
+++ src/index.js
@@ -62,6 +62,6 @@ app.get( '/frame',  async ( request, response ) => {
 	response.send( html );
 });
 
-app.listen( port );
+app.listen( port, () => { console.log( `Listening at http://localhost:${port}`)} );
 
 module.exports = app;
