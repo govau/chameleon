1d7ea15befa097bee23775364f7f79c8d00e61df
Adam A. Zerella <adamzerella@users.noreply.github.com>
2019-01-08 10:23:53 +1100

    Moved from newline to quotes

--- test/unit/slack.spec.js
+++ test/unit/slack.spec.js
@@ -34,7 +34,7 @@ Describe( 'GenerateChameleonMessage()', () => {
 		const path = '/chameleon/form/';
 		const message = GenerateChameleonMessage( url, path, query );
 
-		let fixture = '\n\n_Karma-Karma-Karma-Chameleon!_\n\nGenerating *form* page template\n\n';
+		let fixture = '>_Karma-Karma-Karma-Chameleon!_\n\nGenerating *form* page template\n\n';
 		fixture += '`action`: #800080\n`background`: #FF0000\n';
 		fixture += `Preview: ${url}`;
 
